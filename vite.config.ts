d58c01f5040d5ee451f821002fa090108ded02c5
gonzalo-novak <monstertrap@protonmail.com>
2022-10-29 18:48:42 -0500

    chore: configure vitest

--- vite.config.ts
+++ vite.config.ts
@@ -1,3 +1,6 @@
+/// <reference types="vitest" />
+/// <reference types="vite/client" />
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
@@ -8,5 +11,13 @@ export default defineConfig({
     modules: {
       localsConvention: 'camelCaseOnly',
     }
-  }
+  },
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: ['jest.setup.js'],
+		coverage: {
+			include: ['src/**/*.{ts,tsx}']
+		}
+  },
 })
