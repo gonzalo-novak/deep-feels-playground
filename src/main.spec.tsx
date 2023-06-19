73bd5cbad28d52e56b65629a34a9cc4f5d035187
Roberto P <roberto.pinon@proton.me>
2023-06-19 00:33:15 -0600

    test: moods endpoint

--- src/main.spec.tsx
+++ src/main.spec.tsx
@@ -2,6 +2,9 @@ import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import { createMemoryRouter, RouterProvider } from 'react-router-dom';
 import { ROUTES, routes } from './routes';
+import { server } from './mocks/server';
+import { generateURL } from './mocks/handlers';
+import { rest } from 'msw';
 
 describe('Component: Router', () => {
 	it('Should render pages correctly', async () => {
@@ -19,6 +22,9 @@ describe('Component: Router', () => {
 		const homeTitleEl = await screen.findByRole('heading', { name: /Welcome to Docker \+ Vite/i });
 		expect(homeTitleEl).toBeInTheDocument();
 
+		const didFetchEl = await screen.findByRole('heading', { name: /Data loaded/i });
+		expect(didFetchEl).toBeInTheDocument();
+
 		const registerBtn = await screen.findByRole('button', { name: /Regístrate/i });
 		expect(registerBtn).toBeInTheDocument();
 		await user.click(registerBtn);
@@ -29,5 +35,14 @@ describe('Component: Router', () => {
 		const loginBtn = await screen.findByRole('button', { name: /Inicia Sesión/i });
 		expect(loginBtn).toBeInTheDocument();
 		await user.click(loginBtn);
+	});
+
+	it('Should render an error if the moods fetching fail', async () => {
+		const route = createMemoryRouter(routes, { initialEntries: [ROUTES.HOME], initialIndex: 0 });
+		server.use(rest.get(generateURL('moods'), (_, res, ctx) => res(ctx.json({ ok: false, data: { message: 'Hello MSW!'} }))))
+		render(<RouterProvider router={route}></RouterProvider>);
+		
+		const homeTitleEl = await screen.findByRole('heading', { name: /Hello MSW/i });
+		expect(homeTitleEl).toBeInTheDocument();
 	});
 });
--- src/pages/Login/index.tsx
+++ src/pages/Login/index.tsx
@@ -1,21 +1,30 @@
 import { useNavigate } from 'react-router-dom';
 import { ROUTES } from '../../routes';
+import { useFetch } from '../../hooks/useFetch';
+import { atom, useAtomValue } from 'jotai';
+import { isFetchError } from '../../hooks/useFetch/atoms';
+
+const testMoodsAtom = atom<{_id: string; name: string;} | null>(null);
 
 export const Login = () => {
-
+	useFetch('moods', testMoodsAtom);
+	const moods = useAtomValue(testMoodsAtom);
+	const fetchError = useAtomValue(isFetchError);
 	const navigate = useNavigate();
 
 	const handleRegisterPage = () => {
 		navigate(ROUTES.REGISTER, {
 			replace: true
 		})
-	}	
+	}
+
+	if(fetchError) return <h1>There was an error {fetchError.message}</h1>
 
 	return (
 		<>
 			<h1>Welcome to Docker + Vite</h1>
 			<hr/>
-
+			{(moods) && <h4>Data loaded</h4>}
 			<button onClick={handleRegisterPage}>Regístrate</button>
 		</>
 	)
