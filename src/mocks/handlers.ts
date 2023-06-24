ea77df4abde6ad921549aa90f0e13381a147db0d
Roberto P <roberto.pinon@proton.me>
2023-06-24 15:06:29 -0600

    chore: add needed stuff for testing the login page

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -5,10 +5,12 @@ import { apiContext } from '../hooks/useFetch';
 // Mocks
 import moodsJSON from './moods.mock.json';
 import registerJSON from './register.mock.json';
+import loginJSON from './login.mock.json';
 
 export const generateURL = (endpoint: keyof typeof ENDPOINTS) => process.env.TEST_API_HOST + apiContext + ENDPOINTS[endpoint];
 
 export const handlers = [
   rest.get(generateURL('moods'), (_, res, ctx) => res(ctx.json(moodsJSON))),
 	rest.post(generateURL('register'), (_, res, ctx) => res(ctx.json(registerJSON))),
+	rest.post(generateURL('login'), (_, res, ctx) => res(ctx.json(loginJSON))),
 ];
