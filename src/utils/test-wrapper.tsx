37c66df44ce933d18ba43d9cec346183d76d51ba
Roberto P <roberto.pinon@proton.me>
2023-06-24 14:43:17 -0600

    chore: adapt register tests

--- src/utils/test-wrapper.tsx
+++ src/utils/test-wrapper.tsx
@@ -1,14 +1,15 @@
 import { render } from "@testing-library/react";
 import { createMemoryRouter, RouterProvider } from "react-router-dom";
 import { routes } from "../routes";
+import { ROUTES } from "./routes";
 
-export const testWrapper = (initialEntries: string[], initialIndex?: number) => {
+export const testWrapper = () => {
 	const router = createMemoryRouter(
 		routes, {
 			// It refers to the available routes in this context:
-			initialEntries,
+			initialEntries: Object.values(ROUTES),
 			// The initial router where the application will start.
-			initialIndex: initialIndex || 0,
+			initialIndex: 0,
 		}
 	);
 	return render(<RouterProvider router={router} />);
