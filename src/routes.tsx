15c288d45318da209a375a522e5787e1a2101215
Roberto P <roberto.pinon@proton.me>
2023-06-24 14:14:36 -0600

    chore: add new pages and routes as we'll need them

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,9 +1,12 @@
-import { createBrowserRouter } from 'react-router-dom';
+import { RouteObject, createBrowserRouter } from 'react-router-dom';
 import { Login, Register } from './pages/';
 import { ROUTES } from './utils/routes';
 import { Layout } from './components/Layout';
+import { Moods } from './pages/Moods';
+import { SoundsList } from './pages/SoundsList';
+import { PasswordRecovery } from './pages/PasswordRecovery';
 
-export const routes = [
+export const routes: RouteObject[] = [
 	{
 		path: ROUTES.HOME,
 		element: <Layout />,
@@ -17,7 +20,22 @@ export const routes = [
 				path: ROUTES.REGISTER,
 				element: <Register />
 			},
+			{
+				path: ROUTES.PASSWORD_RECOVERY,
+				element: <PasswordRecovery />
+			},
 		]
 	},
+	{
+		path: ROUTES.PROFILE,
+		element: <Layout />,
+		children: [
+			{ index: true, element: <SoundsList /> },
+			{
+				path: ROUTES.MOODS,
+				element: <Moods />
+			},
+		]
+	}
 ];
 export const router = createBrowserRouter(routes);
