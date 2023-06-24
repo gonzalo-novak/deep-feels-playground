5c0304b5b49df4f1127a5fca91d536cdb3f3678c
Roberto P <roberto.pinon@proton.me>
2023-06-24 14:17:20 -0600

    chore: add elements to be used in our login page

---
+++ src/pages/Login/text.ts
@@ -0,0 +1,17 @@
+export const text = {
+	title: "¡Hola!",
+	subtitle: "Un gusto tenerte de vuelta. Varias experiencias relajantes te están esperando.",
+	main_img_alt: 'Mujer relajada meditando',
+	main_cta: 'Continuar',
+	main_cta_loading: 'Cargando...',
+	form: {
+		email: {
+			label: 'Email',
+			placeholder: 'Escribe tu email'
+		},
+		password: {
+			label: 'Contraseña',
+			placeholder: 'Escribe tu contraseña'
+		}
+	}
+}
