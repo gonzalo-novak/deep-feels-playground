498174f7e519c5e3bb509f4b8e6cb2ff27724b4e
Roberto P <roberto.pinon@proton.me>
2023-06-25 16:27:06 -0600

    feat: moods page

---
+++ src/pages/Moods/text.ts
@@ -0,0 +1,10 @@
+export const text = {
+	title: '¿Tienes alguna de estas emociones?',
+	subtitle: 'Nosotros te ayudaremos a mitigarlo.',
+	main_cta: {
+		if_mood_selected: 'Continuar',
+		if_mood_selected_loading: 'Registrando emociones...',
+		if_no_mood_selected: 'No me siento identificado. Deseo continuar.',
+		if_no_mood_selected_loading: 'Continuando...'
+	},
+};
