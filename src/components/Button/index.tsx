005b67530c9ae52b929c4a93ab1e59fd469fe4c2
Roberto P <roberto.pinon@proton.me>
2023-06-25 16:25:12 -0600

    chore: add purple background to the whole app, and assets for this module

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,8 +1,9 @@
+import classNames from 'classnames';
 import styles from './styles.module.css';
 import { ButtonHTMLAttributes, DetailedHTMLProps, ReactNode } from "react";
 
 export const Button = ({ children, ...rest }: ButtonProps) => (
-	<button className={styles.button} { ...rest }>
+	<button { ...rest } className={classNames(styles.button, rest.className)}>
 		{children}
 	</button>
 );
