07d6f4edf56434202a48a94da6156d1541883641
Roberto P <roberto.pinon@proton.me>
2023-06-26 18:37:37 -0600

    refactor(useFetch): url generation and remove unused code

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -6,11 +6,29 @@ import { apiContext } from '../hooks/useFetch';
 import moodsJSON from './moods.mock.json';
 import registerJSON from './register.mock.json';
 import loginJSON from './login.mock.json';
+import registeredMoodsJSON from './registered-moods.json';
 
-export const generateURL = (endpoint: keyof typeof ENDPOINTS) => process.env.TEST_API_HOST + apiContext + ENDPOINTS[endpoint];
+export const generateURL = (endpoint: keyof typeof ENDPOINTS, params?: { [k: string]: any }) => (
+	// We'll omit this line as we don't need to test it because it's
+	// only used in testing environments
+	/* c8 ignore next */
+	(process.env?.TEST_API_HOST || '')
+	+ apiContext
+	+ (
+		(params)
+		? (
+				Object.entries(params).reduce((acc, [k, v]) => {
+					acc = ENDPOINTS[endpoint].replace(k, String(v))
+					return acc
+				}, '')
+			)
+		: ENDPOINTS[endpoint]
+	)
+);
 
 export const handlers = [
   rest.get(generateURL('moods'), (_, res, ctx) => res(ctx.json(moodsJSON))),
 	rest.post(generateURL('register'), (_, res, ctx) => res(ctx.json(registerJSON))),
 	rest.post(generateURL('login'), (_, res, ctx) => res(ctx.json(loginJSON))),
+	rest.put(generateURL('registerMoods', { id: registerJSON.data.user._id }), (_, res, ctx) => res(ctx.json(registeredMoodsJSON)))
 ];
