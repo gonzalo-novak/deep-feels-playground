3bc8fbba48bce0ec299fd279fda844e460a36471
Roberto P <roberto.pinon@proton.me>
2023-06-29 23:42:50 -0600

    chore(input): add the needed of getting a state from its parent component

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,47 +1,45 @@
-import classNames from 'classnames';
 import styles from './styles.module.css';
-import { ChangeEvent, DetailedHTMLProps, useState } from "react";
+import { ChangeEvent, DetailedHTMLProps, forwardRef, useCallback, useEffect, useRef, useState } from "react";
 
-export const Input = ({
+export const Input = forwardRef(({
 	label,
 	onValidationFn,
-	onChange,
+	setValues,
 	...rest
-}: InputProps) => {
-	const [value, setValue] = useState("");
+}: InputProps, ref: any) => {
+	const didMount = useRef(false);
+	const {defaultValue, ...restProps} = rest;
 	const [errorMessage, setErrorMessage] = useState("");
 
-	const handleInputChange =
-		({ target: { value, name } }: ChangeEvent<HTMLInputElement>) => {
-			setValue(value);
-			onChange({ [name]: value });
-
-			if (onValidationFn) {
-				const error = onValidationFn(value);
-				(typeof error === 'string') ? setErrorMessage(error) : setErrorMessage("");
-			}
+	const handleInputChange = useCallback(({ target: { value, name, type, files } }: ChangeEvent<HTMLInputElement>) => {
+		if (onValidationFn) {
+			const error = onValidationFn(value);
+			(typeof error === 'string') ? setErrorMessage(error) : setErrorMessage("");
 		}
+		setValues((currentVal: { [k: string] : any }) => ({ ...currentVal, [name]: (type === 'file') ? files : value }));
+	}, []);
+
 	return (
 		<div className={rest?.className}>
 			<label className={styles.label}>
 				{label}
 				<input
 					type='text'
-					{...rest}
+					{...restProps}
 					onChange={handleInputChange}
-					value={value}
 					className={(errorMessage) ? styles.inputError : styles.input}
+					ref={ref}
 				/>
 				{(errorMessage) && <span className={styles.errorMessage}>{errorMessage}</span>}
 			</label>
 		</div>
 	);
-}
+})
 
 export type InputProps = DetailedHTMLProps<React.InputHTMLAttributes<HTMLInputElement>, HTMLInputElement> & {
 	label: string;
 	onValidationFn?: TValidationFn;
-	onChange: (value: { [k: string] : string }) => void;
+	setValues: React.Dispatch<React.SetStateAction<any>>;
 }
 
 type TValidationFn = (value: string) => string | null;
