ec77327ebd80754c05c3f39416a61b9e18789de8
Roberto P <roberto.pinon@proton.me>
2023-06-30 00:23:13 -0600

    test: log out

--- src/pages/SoundsList/sound-list.spec.tsx
+++ src/pages/SoundsList/sound-list.spec.tsx
@@ -21,9 +21,20 @@ describe('Page: Sound List', () => {
 
 		expect(await screen.findByRole('heading', { name: 'Hola ' + loggedUserMock.data.user.name })).toBeInTheDocument();
 		expect(await screen.findByText(text.subtitle)).toBeInTheDocument();
+	});
+	it('Should logout correctly', async () => {
+		const user = userEvent.setup();
+		const store = createStore();
+		store.set(sessionAtomWithPersistence, loggedUserMock.data.token);
+		store.set(userAtom, loggedUserMock.data.user)
+		testWrapper(store);
+		await waitForLoadingToFinish();
 
-		const profileButtonEl = await screen.findByLabelText(`Change ${loggedUserMock.data.user.name}'s profile data`);
-		await user.click(profileButtonEl);
-		expect(await screen.findByRole('heading', { name: 'Tu perfil'})).toBeInTheDocument();
+		const soundCardItemEl = await screen.findByAltText('Start playing Jazz Coffee');
+		await user.click(soundCardItemEl);
+
+		expect(await screen.findByRole('heading', { name: 'Hola ' + loggedUserMock.data.user.name })).toBeInTheDocument();
+		await user.click(await screen.findByAltText('Log out'));
+		expect(await screen.findByText('Un gusto tenerte de vuelta. Varias experiencias relajantes te están esperando.')).toBeInTheDocument();
 	});
 });
